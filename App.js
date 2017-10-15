2660516d44f3effca33f69375a2249451c334947
Indrek Lasn <lasnindrek@gmail.com>
2017-10-15 19:45:47 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Header } from './src/components';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header />
+      </View>
+    );
+  }
+}
