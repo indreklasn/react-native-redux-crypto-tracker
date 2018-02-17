de60734b788b53c3cff3b9a818381510186ce2cd
Indrek Lasn <lasnindrek@gmail.com>
2018-02-17 18:21:45 +0100

    Update CoinIcons.js

--- src/Utils/CoinIcons.js
+++ src/Utils/CoinIcons.js
@@ -15,5 +15,6 @@ export const images = {
     XLM: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1516326886/xlm_2x_jfwlwt.png',
     ADA: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1516326874/ada_2x_g4fs0c.png',
     IOTA: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1516327102/miota_2x_zsvtqc.png',
-    TRX: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1516326885/trx_2x_ukhxjm.png'
+    TRX: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1516326885/trx_2x_ukhxjm.png',
+    EOS: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1516326878/eos_2x_dvr7p0.png'
 };
